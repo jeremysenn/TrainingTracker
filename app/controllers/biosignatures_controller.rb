b6bd9e89630507d3e3b859bc056725832f06d508
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2011-08-29 10:26:21 -0700

    sort biosignatures by date, not creation date (since sometimes add later)

--- app/controllers/biosignatures_controller.rb
+++ app/controllers/biosignatures_controller.rb
@@ -15,26 +15,26 @@ class BiosignaturesController < ApplicationController
     @biosignature = Biosignature.new(:client_id => @client.id, :date => Date.today)
     ### AUTOFILL WITH LAST BIOSIG NUMBERS ###
     unless @client.biosignatures.blank?
-      @biosignature.sex = @client.biosignatures.last.sex
-      @biosignature.age = @client.biosignatures.last.age
-      @biosignature.height = @client.biosignatures.last.height
-      @biosignature.height_units = @client.biosignatures.last.height_units
-      @biosignature.weight = @client.biosignatures.last.weight
-      @biosignature.weight_units = @client.biosignatures.last.weight_units
-      @biosignature.chin = @client.biosignatures.last.chin
-      @biosignature.cheek = @client.biosignatures.last.cheek
-      @biosignature.pec = @client.biosignatures.last.pec
-      @biosignature.tri = @client.biosignatures.last.tri
-      @biosignature.subscap = @client.biosignatures.last.subscap
-      @biosignature.suprailiac = @client.biosignatures.last.suprailiac
-      @biosignature.midaxil = @client.biosignatures.last.midaxil
-      @biosignature.umbilical = @client.biosignatures.last.umbilical
-      @biosignature.knee = @client.biosignatures.last.knee
-      @biosignature.calf = @client.biosignatures.last.calf
-      @biosignature.quad = @client.biosignatures.last.quad
-      @biosignature.ham = @client.biosignatures.last.ham
-      @biosignature.waist = @client.biosignatures.last.waist
-      @biosignature.hip = @client.biosignatures.last.hip
+      @biosignature.sex = @client.biosignatures.sort_by(&:date).reverse.last.sex
+      @biosignature.age = @client.biosignatures.sort_by(&:date).reverse.last.age
+      @biosignature.height = @client.biosignatures.sort_by(&:date).reverse.last.height
+      @biosignature.height_units = @client.biosignatures.sort_by(&:date).reverse.last.height_units
+      @biosignature.weight = @client.biosignatures.sort_by(&:date).reverse.last.weight
+      @biosignature.weight_units = @client.biosignatures.sort_by(&:date).reverse.last.weight_units
+      @biosignature.chin = @client.biosignatures.sort_by(&:date).reverse.last.chin
+      @biosignature.cheek = @client.biosignatures.sort_by(&:date).reverse.last.cheek
+      @biosignature.pec = @client.biosignatures.sort_by(&:date).reverse.last.pec
+      @biosignature.tri = @client.biosignatures.sort_by(&:date).reverse.last.tri
+      @biosignature.subscap = @client.biosignatures.sort_by(&:date).reverse.last.subscap
+      @biosignature.suprailiac = @client.biosignatures.sort_by(&:date).reverse.last.suprailiac
+      @biosignature.midaxil = @client.biosignatures.sort_by(&:date).reverse.last.midaxil
+      @biosignature.umbilical = @client.biosignatures.sort_by(&:date).reverse.last.umbilical
+      @biosignature.knee = @client.biosignatures.sort_by(&:date).reverse.last.knee
+      @biosignature.calf = @client.biosignatures.sort_by(&:date).reverse.last.calf
+      @biosignature.quad = @client.biosignatures.sort_by(&:date).reverse.last.quad
+      @biosignature.ham = @client.biosignatures.sort_by(&:date).reverse.last.ham
+      @biosignature.waist = @client.biosignatures.sort_by(&:date).reverse.last.waist
+      @biosignature.hip = @client.biosignatures.sort_by(&:date).reverse.last.hip
 
     end
   end
