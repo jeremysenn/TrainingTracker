f4a4bd44daad4adfaaf78154decd0d145fc8d2e1
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2012-02-17 13:07:19 -0800

    Errors when working with Cara

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,28 @@ class ApplicationController < ActionController::Base
 
 #  check_authorization
 
+  before_filter :prepare_for_mobile
+
   rescue_from CanCan::AccessDenied do |exception|
 #    redirect_to root_url, :alert => exception.message
     redirect_to root_url, :alert => "That's not a page you have access to"
+  end
+
+  private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
   end
 
 end
