e49fdd423cd47f2b66f6bad67a8cbf350c2e74bb
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2012-03-06 18:24:41 -0800

    Workout name checkbox to choose new or existing; changed sign up value of is_trainer or is_gym or is_client to 1 instead of true to see if that works for hosted site.

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -13,3 +13,16 @@ $(function() {
     return confirm("Delete this item?");
   });
 });
+
+function toggleStatus(r) {
+  var new_inputs = $("#existing_workout_" + r).closest("form").find('#new_' + r).children("input");
+  var old_inputs = $("#existing_workout_" + r).closest("form").find('#old_' + r).children("input,select")
+
+  if ($("#existing_workout_" + r).is(':checked')) {
+    new_inputs.attr('disabled', true);
+    old_inputs.removeAttr('disabled');
+  } else {
+      old_inputs.attr('disabled', true);
+      new_inputs.removeAttr('disabled');
+  }
+}
