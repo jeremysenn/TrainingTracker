838ba525ec69cdd74ed118e911e513934fec539f
Jeremy Senn <senn.jeremy@gmail.com>
2014-11-16 09:59:34 -0800

    testing login from mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,9 @@ class ApplicationController < ActionController::Base
   
   helper_method :mobile_device?
 
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
+#  def prepare_for_mobile
+#    session[:mobile_param] = params[:mobile] if params[:mobile]
+#    request.format = :mobile if mobile_device?
+#  end
 
 end
