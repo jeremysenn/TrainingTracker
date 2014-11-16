29dde2866ae03230208639c126212219efe19184
Jeremy Senn <senn.jeremy@gmail.com>
2014-11-16 09:38:54 -0800

    testing login from mobile

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -20,6 +20,7 @@ module Authentication
   end
   
   def current_user
+    logger.debug "user_id: #{session[:user_id]}"
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
